5e6592e9db7bb3e49f5dd992985149e9556c449f
priya <priyasm029@gmail.com>
2021-04-01 01:36:57 +0530

    check

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
-var platform,bird2
+var platform,bird2;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
